8500f2c74c9bf6380a28ed89dca24a9e53b931c3
n_hel <n_hel@GOLLUM>
2017-06-23 23:08:20 +0200

    test für flo

--- NXTProject/src/test/HelloWorld.java
+++ NXTProject/src/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("hallo ich bin alex und ich mag bananen.");
-		System.out.println("Endlich geht der Sch***");
+		System.out.println("Endlich geht der Mist");
 
 	}
 
