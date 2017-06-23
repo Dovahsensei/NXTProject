013219aa4aa70c24dc57abe470da7a6bf7edad9b
Alectade00 <afrey338@gmail.com>
2017-06-23 23:02:42 +0200

    hallo test

--- NXTProject/src/test/HelloWorld.java
+++ NXTProject/src/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println();
+		System.out.println("hallo ich bin alex und ich mag bananen.");
 
 	}
 
