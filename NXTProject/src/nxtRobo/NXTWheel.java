41dc741431cbb226f445f42acf579083fca47f78
Florian <Florian@192.168.0.241>
2017-07-13 23:41:32 +0200

    Versuch es zum Laufen zu kriegen

--- NXTProject/src/nxtRobo/NXTWheel.java
+++ NXTProject/src/nxtRobo/NXTWheel.java
@@ -3,9 +3,9 @@ package nxtRobo;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.TachoMotorPort;
 
-public class NXTWheel extends NXTRegulatedMotor{
+public class NXTWheel extends NXTRegulatedMotor{S
 	
-	final int normalVelocity = 150;
+	final int normalVelocity = 150; //Geschwindigkeit in rad/min
 	
 	public NXTWheel(TachoMotorPort port){
 		super(port);
